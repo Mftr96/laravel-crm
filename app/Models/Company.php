8dbee6d18dce1831fe15e98bd3c504f82b8f779f
Mftr96 <matteo.federici96@gmail.com>
2025-01-23 10:36:44 +0100

    making milestone 4; created Company Index

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Employee;
+use Orchid\Screen\AsSource;
 
-class Company extends Model
-{
-    protected $fillable = ['name', 'email', 'logo', 'VAT_number'];
+class Company extends Model {
+    use AsSource;
+    protected $fillable = [
+        'name',
+        'email',
+        'logo',
+        'VAT_number'
+    ];
 
     public function employees()
     {
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Company;
+use Orchid\Screen\AsSource;
 
 class Employee extends Model
 {
-    protected $fillable = ['first_name', 'last_name', 'company_id', 'email', 'phone'];
+    use AsSource;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'company_id',
+        'email',
+        'phone'
+    ];
 
     public function company()
     {
